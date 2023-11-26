8ec16140b9c049e16b029b682e695536a8fd207d
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-26 17:09:50 +0100

    Word image

--- app/src/main/java/otus/gpb/dzrazbor/MainActivity.kt
+++ app/src/main/java/otus/gpb/dzrazbor/MainActivity.kt
@@ -2,10 +2,21 @@ package otus.gpb.dzrazbor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordImg: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        wordImg = findViewById(R.id.img_word)
+        setImage()
+    }
+
+    private fun setImage() {
+        wordImg.setImageResource(R.drawable.img_restaurant)
     }
 }
