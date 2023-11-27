7c114751355bb31d5836afb5477f237868d01be9
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-27 08:29:12 +0100

    Answer words

---
+++ app/src/main/java/otus/gpb/dzrazbor/Answer.kt
@@ -0,0 +1,3 @@
+package otus.gpb.dzrazbor
+
+data class Answer(val word: String, val correct: Boolean = false)
--- app/src/main/java/otus/gpb/dzrazbor/MainActivity.kt
+++ app/src/main/java/otus/gpb/dzrazbor/MainActivity.kt
@@ -3,20 +3,49 @@ package otus.gpb.dzrazbor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var wordImg: ImageView
+    private lateinit var answersAdapter: AnswersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        wordImg = findViewById(R.id.img_word)
-        setImage()
+        setupView()
+        setData()
     }
 
-    private fun setImage() {
+    private fun setupView() {
+        wordImg = findViewById(R.id.img_word)
+        setupAnswers()
+    }
+
+    private fun setData() {
         wordImg.setImageResource(R.drawable.img_restaurant)
+        answersAdapter.submitList(
+            listOf(
+                Answer("Магазин"),
+                Answer("Больница"),
+                Answer("АЗС"),
+                Answer("Ресторан", true),
+            )
+        )
+    }
+
+    private fun setupAnswers() {
+        answersAdapter = AnswersAdapter()
+        findViewById<RecyclerView>(R.id.answers).apply {
+            adapter = answersAdapter
+            layoutManager = GridLayoutManager(
+                this@MainActivity,
+                2,
+                RecyclerView.VERTICAL,
+                false
+            )
+        }
     }
 }
